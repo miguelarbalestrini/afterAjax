3f6ab419599306595dae794e01b068c92538a574
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2022-05-06 18:57:35 -0300

    after class updated with fetch

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # afterAjax
+Links: 
+
+- API: https://pokeapi.co/
+
+- CDN AJAX: https://releases.jquery.com/
+
+- json viewer: https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh/related?hl=es
+
+
--- js/index.js
+++ js/index.js
@@ -1,38 +1,67 @@
-let pokeID = $("#pokeID");
+const pokeID = document.getElementById("pokeID");
 const APIURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 
-const getData = (pokemon) => {
+const getDataAjax = () => {
     $.ajax({
         method: "GET",
-        url: APIURL + `${pokemon}/`,
+        url: APIURL,
         success: (respuesta) => {
-            // console.log(respuesta);
-            // console.log(respuesta.sprites.front_default);
-            // $("body").prepend(`<img src='${respuesta.sprites.front_default}'></img>`);
-            // data = JSON.parse(respuesta);
-            renderData((respuesta));
+            console.log(respuesta);
         },
         error: (state, responseText) => {
             console.log(state);
-            alert(` ${responseText}, El server respondió con ${state.status}`)
-
+            Swal.fire({
+                icon: 'error',
+                title: 'Algo salió mal',
+                text: `${responseText}, El server respondió con ${state.status}`,
+            })
         }
     });
+}
+
+const getDataFetch = (pokemon) => {
+    fetch(`${APIURL}${pokemon}`)
+        .then(response => response.json())
+        .then(data => renderData(data))
+        .catch((error) => {
+            console.log(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Algo salió mal',
+                text: ` ${error}`,
+            })
+        })
+}
+
+const getDataFetchAsync = async (pokemon) => {
+    try {
+        let response = await fetch(`${APIURL}${pokemon}`);
+        let data = await response.json();
+        renderData(data);
+    }
+    catch(error) {
+        console.log(error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Algo salió mal',
+            text: ` ${error}`,
+        })
+    }
 }
 
 $(document).ready(function () {
     //Declaramos la url del API
     //Agregamos un botón con jQuery
     //Escuchamos el evento click del botón agregado
-    // getData();
+    // getDataAjax();
 });
 
 let counter = 0;
 const renderData = (pokemon) => {
     if (pokemon != undefined) {
-        const pokedex = $('#pokedex');
-        pokedex.prepend(`
+        const pokedex = document.getElementById('pokedex');
+        pokedex.innerHTML += (`
         <div class="card" style="width: 18rem;">
             <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -48,13 +77,18 @@ const renderData = (pokemon) => {
     }
 }
 
-$('#render').click(() => {
-    console.log(pokeID[0].value, 'VALUE')
-    let pokemonID = Number(pokeID[0].value);
+document.getElementById('render').onclick = (() => {
+    console.log(pokeID.value, 'VALUE')
+    let pokemonID = Number(pokeID.value);
     if (Number.isInteger(pokemonID) && pokemonID > 0 && pokemonID < 899) {
-        getData(pokemonID);
+        // getDataFetch(pokemonID);
+        getDataFetchAsync(pokemonID);
     }
     else {
-        alert("Tiene que ser un entero entre 1 y 898!");
+        Swal.fire({
+            icon: 'error',
+            title: 'No ingresaste un pokemon válido',
+            text: 'Tiene que ser un entero entre 1 y 898!',
+        })
     }
 });
